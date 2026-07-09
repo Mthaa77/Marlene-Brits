c389c36569ed9b0c125f06afd00c06b312241120
Mthaa77 <tshepisokadiaka83@gmail.com>
2026-07-09 06:08:34 +0200

    Load full-site premium visual upgrades

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Playfair_Display, Inter, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 import "./mobile-fixes.css";
+import "./premium-upgrades.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 
